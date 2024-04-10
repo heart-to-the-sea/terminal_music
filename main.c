1d4af42db5de2e2db5ba77f526b0c1b771aeff45
heart-to-the-sea <x1911898144@163.com>
2024-04-10 20:07:37 +0800

    append pdf

--- main.c
+++ main.c
@@ -3,16 +3,25 @@
 // build gcc main.c -lncurses
 int main(int argc, char const *argv[])
 {
+  int ch;
   initscr(); // 获取终端特征如大小等，并设置终端支持的curses环境
+  raw();
   cbreak(); // 禁用缓冲区,并设置curses每次只接受一个字符
   noecho(); // 告诉curses不要把输入回显到屏幕上
   clear(); // 清空当前屏幕
-  int scrLine, scrCol;
-  getmaxyx(stdscr, scrLine, scrCol);
-  printf("%d %d \n",scrLine,scrCol);
-  mvaddstr(scrLine-1,0,"hello world");
 
-  getch(); // 等待用户输入
+  ch = getch();
+
+  if(ch == 'A') {
+    printw("F1 key pressed");
+  } else {
+    printw("The pressed key is ");
+    attron(A_BOLD);
+    printw("%c", ch);
+    attroff(A_BOLD);
+  }
+  refresh();
+  getch();
   endwin(); // 返回控制台
   
   return 0;
