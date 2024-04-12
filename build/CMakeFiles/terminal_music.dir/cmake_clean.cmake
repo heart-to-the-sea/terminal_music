72169e561c5664a256f1ddb8a11c5ae426cb9bad
heart-to-the-sea <x1911898144@163.com>
2024-04-12 14:36:27 +0800

    init

---
+++ build/CMakeFiles/terminal_music.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terminal_music.dir/main.cpp.o"
+  "CMakeFiles/terminal_music.dir/main.cpp.o.d"
+  "terminal_music"
+  "terminal_music.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/terminal_music.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
