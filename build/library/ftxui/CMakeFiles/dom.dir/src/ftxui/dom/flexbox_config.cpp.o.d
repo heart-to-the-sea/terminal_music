d4f5dd3e35945555ad2ad82f9635daf779611d48
Your Name <you@example.com>
2024-04-15 08:14:15 +0800

    init

--- build/library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/include/ftxui/dom/flexbox_config.hpp
