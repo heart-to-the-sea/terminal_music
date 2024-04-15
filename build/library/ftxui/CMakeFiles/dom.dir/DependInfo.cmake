b6ac177964d157bd38300a6dccf17b2f33cba4ae
Your Name <you@example.com>
2024-04-15 08:12:55 +0800

    init

---
+++ build/library/ftxui/CMakeFiles/dom.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/automerge.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/automerge.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/automerge.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/blink.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/blink.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/blink.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/bold.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/bold.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/bold.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/border.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/border.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/border.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/box_helper.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/box_helper.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/box_helper.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/canvas.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/canvas.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/canvas.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/clear_under.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/clear_under.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/clear_under.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/color.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/color.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/color.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/composite_decorator.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/composite_decorator.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/composite_decorator.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/dbox.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/dbox.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/dbox.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/dim.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/dim.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/dim.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/flex.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flex.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flex.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/flexbox.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/flexbox_config.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/flexbox_helper.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_helper.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_helper.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/focus.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/focus.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/focus.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/frame.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/frame.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/frame.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/gauge.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/gauge.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/gauge.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/graph.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/graph.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/graph.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/gridbox.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/gridbox.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/gridbox.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/hbox.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/hbox.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/hbox.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/hyperlink.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/hyperlink.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/hyperlink.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/inverted.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/inverted.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/inverted.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/linear_gradient.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/linear_gradient.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/linear_gradient.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/node.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/node.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/node.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/node_decorator.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/node_decorator.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/node_decorator.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/paragraph.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/paragraph.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/paragraph.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/reflect.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/reflect.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/reflect.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/scroll_indicator.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/scroll_indicator.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/scroll_indicator.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/separator.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/separator.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/separator.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/size.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/size.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/size.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/spinner.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/spinner.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/spinner.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/strikethrough.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/strikethrough.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/strikethrough.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/table.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/table.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/table.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/text.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/text.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/text.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/underlined.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/underlined.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/underlined.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/underlined_double.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/underlined_double.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/underlined_double.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/util.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/util.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/util.cpp.o.d"
+  "/home/xingqiankun/workspace/c_workspace/terminal_music/library/ftxui/src/ftxui/dom/vbox.cpp" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/vbox.cpp.o" "gcc" "library/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/vbox.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
