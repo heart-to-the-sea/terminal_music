da0662fdbc223bb39a989f880180e2128ac76178
heart-to-the-sea <x1911898144@163.com>
2024-04-16 17:29:07 +0800

    analizy componse base

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,38 @@
-#include <iostream>
-#include <ftxui/component/component.hpp>
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/dom/elements.hpp>
+// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#include <memory> // for shared_ptr, __shared_ptr_access
+#include <string> // for string, basic_string, operator+, to_string
+#include <vector> // for vector
+
+#include "ftxui/component/captured_mouse.hpp"     // for ftxui
+#include "ftxui/component/component.hpp"          // for Radiobox, Renderer
+#include "ftxui/component/component_base.hpp"     // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"                 // for operator|, Element, size, border, frame, HEIGHT, LESS_THAN
 
 using namespace ftxui;
 
-// 自定义按钮组件
-class CustomButton : public ComponentBase
-{
-public:
-  CustomButton(std::wstring label, std::function<void()> onclick)
-      : label_(std::move(label)), onclick_(std::move(onclick)) {}
-
-  // 更新按钮标签
-  void SetLabel(const std::wstring &label)
-  {
-    label_ = label;
-    Render();
-  }
-
-  Element Render() override
-  {
-    // 渲染按钮
-    return hbox({
-        text(L"[ "),
-        text(label_) | size(WIDTH, EQUAL, label_.size() + 2),
-        text(L" ]"),
-    });
-  }
-
-  bool OnEvent(Event event) override
-  {
-    // 处理点击事件
-    if (event.is_mouse())
-    {
-      std::cout<<"--------------->"<<std::endl;
-      onclick_();
-      return true;
-    }
-    return false;
-  }
-
-private:
-  std::wstring label_;
-  std::function<void()> onclick_;
-};
-
 int main()
 {
-  // 创建 ScreenInteractive 对象
-  auto screen = ScreenInteractive::Fullscreen();
+  std::vector<std::string> entries;
+  int selected = 4;
+  std::string str="--\"\"--"+selected;
+  Button("", []{});
+  auto txt = text(str);
 
-  // 创建自定义按钮组件
-  auto button = std::make_shared<CustomButton>(L"My Button", [&] {
-
+  for (int i = 0; i < 30; ++i)
+    entries.push_back("Entry " + std::to_string(i));
+  auto radiobox = Menu(&entries,&selected,{
+    
   });
 
-  // 将按钮添加到屏幕
-  screen.Loop(button);
+  auto renderer = Renderer(radiobox, [&]
+                           { return vbox({txt, vbox({radiobox->Render()}) | frame |
+                                                   size(HEIGHT, LESS_THAN, 10) | border}); });
+
+  auto screen = ScreenInteractive::FitComponent();
+  screen.Loop(renderer);
 
   return 0;
 }
